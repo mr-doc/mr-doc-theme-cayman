23c8f21cfb368623d402990c534b79067df44a8f
iwatakeshi <iwatakeshi@gmail.com>
2015-09-07 13:41:28 -0600

    Update
    Fixes #1 and #3.
    Added minification into gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var shell       = require('gulp-shell');
 var connect     = require('gulp-connect');
 var sass        = require('gulp-sass');
 var clipboard   = require('gulp-clipboard');
+var uglify      = require('gulp-uglify');
+var uglifycss   = require('gulp-uglifycss');
 var path        = require('path');
 var _           = require('lodash');
 
@@ -21,7 +23,7 @@ gulp.task('docs',shell.task([
 
 
 
-// Task 2: Build Sass and copy it into docs
+// Task 2: Build Sass and copy it into docs and assets
 gulp.task('sass', ['docs'], function() {
     return gulp.src("./scss/*.scss")
         .pipe(sass({
@@ -30,10 +32,14 @@ gulp.task('sass', ['docs'], function() {
             ]
         }))
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest("docs/css/"));
+        .pipe(uglifycss({
+          'max-line-len': 80
+        }))
+        .pipe(gulp.dest("docs/css/"))
+        .pipe(gulp.dest("assets/css/"));
 });
 
-// Task 3: Copy the files from bower into js
+// Task 3: Copy the files from bower into js and assets
 gulp.task('copy:js', ['docs'], function () {
     return gulp.src([
       bower.prism.path + 'prism.js',
@@ -43,8 +49,10 @@ gulp.task('copy:js', ['docs'], function () {
       bower.bootstrap.path + 'assets/javascripts/bootstrap.min.js',
       bower.jquery.path + 'dist/jquery.min.js'
         ])
+    .pipe(uglify())
     .pipe(clipboard())
-    .pipe(gulp.dest("docs/js/"));
+    .pipe(gulp.dest("docs/js/"))
+    .pipe(gulp.dest('assets/js'));
 });
 
 // Task 4: Copy the files from bower into css
@@ -53,6 +61,9 @@ gulp.task('copy:css', ['docs'], function () {
       bower.prism.path + 'themes/prism.css'
     ])
     .pipe(clipboard())
+    .pipe(uglifycss({
+      'max-line-len': 80
+    }))
     .pipe(gulp.dest("docs/css/"));
 });
 
@@ -76,6 +87,6 @@ gulp.task('watch', function () {
 });
 
 // Default
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['build','connect', 'watch']);
 // Build
 gulp.task('build', ['docs', 'sass','copy:js', 'copy:css']);
--- test/index.js
+++ test/index.js
@@ -1,8 +1,21 @@
 /**
  * @class Doxx
  */
-class Doxx {
+class Doxx extends Compiler {
 	constructor(){
+
+	}
+	/**
+	 * Generates the files
+	 */
+	generate (){
+
+	}
+
+	/** 
+	 * Some other function
+	 */
+	fn () {
 
 	}
 }
