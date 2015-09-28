40312918125da50724975bf93c1c8a6d423e8d43
iwatakeshi <iwatakeshi@gmail.com>
2015-09-27 20:13:15 -0600

    Fix
    Fixed offset issue.

--- test/source/api/api.js
+++ test/source/api/api.js
@@ -18,4 +18,77 @@ class Doxx extends Compiler {
 	fn () {
 
 	}
+	/** 
+	 * Some other function
+	 */
+	fn2 () {
+
+	}
+	/** 
+	 * Some other function
+	 */
+	fn3 () {
+
+	}
+	/** 
+	 * Some other function
+	 */
+	fn4 () {
+
+	}
+	/** 
+	 * Some other function
+	 */
+	fn5(){
+	}
+	/** 
+	 * Some other function
+	 */
+	fn6 () {
+
+	}
+	/** 
+	 * Some other function
+	 */
+	fn7 () {
+
+	}
+	/** 
+	 * Some other function
+	 */
+	fn8 () {
+
+	}
+	/** 
+	 * Some other function
+	 */
+	fn9 () {
+
+	}
+	/** 
+	 * Some other function
+	 */
+	fn10 () {
+
+	}
+	/** 
+	 * Some other function
+	 */
+	fn11 () {
+
+	}
+	/** 
+	 * Some other function
+	 */
+	fn12 () {
+
+	}
+	/** 
+	 * Some other function
+	 */
+	fn13 () {
+
+	}
+	
+	
 }
